7b3c6d23f72a7b49575e76fc5c5746f5bcedcad1
refcell <abigger87@gmail.com>
2024-10-18 10:19:55 -0400

    maps

--- lua/src/maps.lua
+++ lua/src/maps.lua
@@ -5,6 +5,9 @@ local map = vim.keymap.set
 map("n", "<C-d>", "<C-d>zz", opts)
 map("n", "<C-u>", "<C-u>zz", opts)
 
+-- Copy to clipboard
+map("v", "<leader>y", '"+y', opts)
+
 -- Move selected line / block of text in visual mode
 map("v", "J", ":m '>+1<CR>gv=gv", opts)
 map("v", "K", ":m '<-2<CR>gv=gv", opts)
@@ -31,6 +34,14 @@ map("n", "[b", ":bprevious<CR>", opts)
 -- Organize buffers
 map("n", ">b", ":BufferLineMoveNext<CR>", opts)
 map("n", "<b", ":BufferLineMovePrev<CR>", opts)
+
+-- Files --
+map("n", "<leader>w", "<cmd>w<cr>", { silent = true, desc = "Save" })
+map("n", "<leader>q", "<cmd>confirm q<cr>", { silent = true, desc = "Quit" })
+map("n", "<leader>n", "<cmd>enew<cr>", { silent = true, desc = "New File"})
+map("n", "<C-n>", "<cmd>enew<cr>", { silent = true, desc = "New File"})
+map("n", "<C-W>", "<cmd>w!<cr>", { silent = true, desc = "Force write"})
+map("n", "<C-q>", "<cmd>qa!<cr>", { silent = true, desc = "Force quit all"})
 
 -- paste over currently selected text without yanking it
 map("v", "p", '"_dp')
