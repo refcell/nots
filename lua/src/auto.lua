335ce9602ffaae23089dfa02d5f56ed888faae34
refcell <abigger87@gmail.com>
2024-10-23 13:25:28 -0400

    chore: cleanup auto commands

--- lua/src/auto.lua
+++ lua/src/auto.lua
@@ -1,162 +1,167 @@
-local api = vim.api
+-- Autocommands
 
 local colors = {
   fg = "#76787d",
   bg = "#252829",
 }
 
--- don't auto comment new line
-api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
-api.nvim_create_autocmd("BufEnter", {
-  callback = function() end,
-})
-
---- Remove all trailing whitespace on save
--- local TrimWhiteSpaceGrp = api.nvim_create_augroup("TrimWhiteSpaceGrp", { clear = true })
--- api.nvim_create_autocmd("BufWritePre", {
---   command = [[:%s/\s\+$//e]],
---   group = TrimWhiteSpaceGrp,
--- })
-
--- wrap words "softly" (no carriage return) in mail buffer
-api.nvim_create_autocmd("Filetype", {
-  pattern = "mail",
-  callback = function()
-    vim.opt.textwidth = 0
-    vim.opt.wrapmargin = 0
-    vim.opt.wrap = true
-    vim.opt.linebreak = true
-    vim.opt.columns = 80
-    vim.opt.colorcolumn = "80"
-  end,
-})
-
--- Autocommand to check the color scheme on startup
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    require("utils.utils").check_and_update_color_scheme()
-  end,
-})
-
+-- Updates the color scheme every 10 seconds
 vim.fn.timer_start(10000, function()
   require("utils.utils").check_and_update_color_scheme()
 end, { ["repeat"] = -1 })
 
--- Highlight on yank
-api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
--- go to last loc when opening a buffer
-api.nvim_create_autocmd("BufReadPost", {
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
-})
-
-api.nvim_create_autocmd("WinEnter", {
-  command = [[set colorcolumn=120]],
-})
-
-api.nvim_create_autocmd("WinLeave", {
-  command = [[set colorcolumn=0]],
-})
-
-api.nvim_create_autocmd("FileType", { pattern = "man", command = [[nnoremap <buffer><silent> q :quit<CR>]] })
-
--- show cursor line only in active window
-local cursorGrp = api.nvim_create_augroup("CursorLine", { clear = true })
-api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+-- Show cursor line only in active window
+local cursorGrp = vim.api.nvim_create_augroup("CursorLine", { clear = true })
+vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
   pattern = "*",
   command = "set cursorline",
   group = cursorGrp,
 })
-api.nvim_create_autocmd(
+vim.api.nvim_create_autocmd(
   { "InsertEnter", "WinLeave" },
   { pattern = "*", command = "set nocursorline", group = cursorGrp }
 )
 
-vim.api.nvim_create_autocmd("ColorScheme", {
-  callback = function()
-    -- change the background color of floating windows and borders.
-    vim.cmd("highlight NormalFloat guibg=none guifg=none")
-    vim.cmd("highlight FloatBorder guifg=" .. colors.fg .. " guibg=none")
-    vim.cmd("highlight NormalNC guibg=none guifg=none")
-
-    -- change neotree background colors
-    -- Default: NeoTreeNormal  xxx ctermfg=223 ctermbg=232 guifg=#d4be98 guibg=#141617
-    -- vim.cmd('highlight NeoTreeNormal guibg=#252e33 guifg=none')
-    -- vim.cmd('highlight NeoTreeFloatNormal guifg=none guibg=none')
-    -- vim.cmd('highlight NeoTreeFloatBorder gui=none guifg=' .. colors.fg .. ' guibg=none')
-    -- vim.cmd('highlight NeoTreeEndOfBuffer guibg=#252e33') -- 1d2021
-
-    vim.cmd("highlight Comment guifg=#475558")
-  end,
-})
-
--- close some filetypes with <q>
-vim.api.nvim_create_autocmd("FileType", {
-  group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
-  pattern = {
-    "PlenaryTestPopup",
-    "help",
-    "lspinfo",
-    "man",
-    "notify",
-    "qf",
-    "spectre_panel",
-    "startuptime",
-    "tsplayground",
-    "neotest-output",
-    "checkhealth",
-    "neotest-summary",
-    "neotest-output-panel",
-  },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-  end,
-})
-
--- resize neovim split when terminal is resized
+-- Resize neovim split when terminal is resized
 vim.api.nvim_command("autocmd VimResized * wincmd =")
 
--- vim.api.nvim_create_autocmd("ColorScheme", {
---   pattern = "kanagawa",
---   callback = function()
---     if vim.o.background == "light" then
---       vim.fn.system("kitty +kitten themes Kanagawa_light")
---     elseif vim.o.background == "dark" then
---       vim.fn.system("kitty +kitten themes Kanagawa_dragon")
---     else
---       vim.fn.system("kitty +kitten themes Kanagawa")
---     end
---   end,
--- })
+local autocmd_dict = {
+  BufEnter = {
+    {
+      -- don't auto comment new line
+      pattern = "*",
+      command = [[set formatoptions-=cro]],
+    },
+    {
+      pattern = "*",
+      callback = function() end,
+    },
+  },
+  Filetype = {
+    {
+      -- Fix terraform and hcl comment string
+      group = vim.api.nvim_create_augroup("FixTerraformCommentString", { clear = true }),
+      callback = function(ev)
+        vim.bo[ev.buf].commentstring = "# %s"
+      end,
+      pattern = { "terraform", "hcl" },
+    },
+    {
+      -- Close some filetypes with <q>
+      group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
+      pattern = {
+        "PlenaryTestPopup",
+        "help",
+        "lspinfo",
+        "man",
+        "notify",
+        "qf",
+        "spectre_panel",
+        "startuptime",
+        "tsplayground",
+        "neotest-output",
+        "checkhealth",
+        "neotest-summary",
+        "neotest-output-panel",
+      },
+      callback = function(event)
+        vim.bo[event.buf].buflisted = false
+        vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+      end,
+    },
+    {
+      -- Remap q to quit
+      pattern = "man",
+      command = [[nnoremap <buffer><silent> q :quit<CR>]],
+    },
+    {
+      -- Wrap words "softly" (no carriage return) in mail buffer
+      pattern = "mail",
+      callback = function()
+        vim.opt.textwidth = 0
+        vim.opt.wrapmargin = 0
+        vim.opt.wrap = true
+        vim.opt.linebreak = true
+        vim.opt.columns = 80
+        vim.opt.colorcolumn = "80"
+      end,
+    },
+  },
+  VimEnter = {
+    {
+      -- Toggle showkeys on startup
+      callback = function()
+        require("showkeys").toggle()
+      end,
+    },
+    {
+      -- Check the color scheme on startup
+      callback = function()
+        require("utils.utils").check_and_update_color_scheme()
+      end,
+    },
+  },
+  TextYankPost = {
+    {
+      -- Highlight on yank
+      callback = function()
+        vim.highlight.on_yank()
+      end,
+    },
+  },
+  BufReadPost = {
+    {
+      -- Go to the last loc when opening a buffer
+      callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+          pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+      end,
+    },
+  },
+  BufWritePre = {
+    {
+      -- Remove trailing whitespace on save
+      command = [[%s/\s\+$//e]],
+    },
+  },
+  WinEnter = {
+    {
+      -- Set colorcolumn to 120
+      command = [[set colorcolumn=120]],
+    },
+  },
+  WinLeave = {
+    {
+      -- Set colorcolumn to 0
+      command = [[set colorcolumn=0]],
+    },
+  },
+  ColorScheme = {
+    {
+      -- Change the background color of floating windows and borders.
+      callback = function()
+        vim.cmd("highlight NormalFloat guibg=none guifg=none")
+        vim.cmd("highlight FloatBorder guifg=" .. colors.fg .. " guibg=none")
+        vim.cmd("highlight NormalNC guibg=none guifg=none")
 
---fix terraform and hcl comment string
-vim.api.nvim_create_autocmd("FileType", {
-  group = vim.api.nvim_create_augroup("FixTerraformCommentString", { clear = true }),
-  callback = function(ev)
-    vim.bo[ev.buf].commentstring = "# %s"
-  end,
-  pattern = { "terraform", "hcl" },
-})
+        -- change neotree background colors
+        -- Default: NeoTreeNormal  xxx ctermfg=223 ctermbg=232 guifg=#d4be98 guibg=#141617
+        -- vim.cmd('highlight NeoTreeNormal guibg=#252e33 guifg=none')
+        -- vim.cmd('highlight NeoTreeFloatNormal guifg=none guibg=none')
+        -- vim.cmd('highlight NeoTreeFloatBorder gui=none guifg=' .. colors.fg .. ' guibg=none')
+        -- vim.cmd('highlight NeoTreeEndOfBuffer guibg=#252e33') -- 1d2021
 
--- vim.api.nvim_create_autocmd({ "OptionSet" }, {
---   pattern = { "background" },
---   callback = function(ev)
---     if vim.o.background == 'dark' then
---       vim.cmd("colorscheme gruvbox-material")
---     else
---       vim.cmd("colorscheme gruvbox-material")
---     end
---     vim.cmd("mode")
---   end
--- })
+        vim.cmd("highlight Comment guifg=#475558")
+      end,
+    },
+  },
+}
+
+for event, opt_tbls in pairs(autocmd_dict) do
+    for _, opt_tbl in pairs(opt_tbls) do
+        vim.api.nvim_create_autocmd(event, opt_tbl)
+    end
+end
